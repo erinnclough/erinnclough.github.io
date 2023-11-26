d2e2c924a81f7d258bca5fa35fddab747ffbcbc7
Erin Clough <erinnclough@Erins-MacBook-Air.local>
2023-11-25 18:13:25 -0800

    update image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 // import {BsFillMoonStarsFill} from "react-icons/bs";
 import  {AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import Image from "next/image";
-import deved from "../public/dev-ed-wave.png";
+//import deved from "/Users/erinnclough/Desktop/portfolio/erinnclough.github.io/public";
 import {useState} from 'react';
 
 
@@ -36,7 +36,7 @@ export default function Home(){
         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/erinclough/"><AiFillLinkedin /></a>   
         </div>
         <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
-          <Image src={deved} layout="fill" objectFit="cover"/>
+         {/* <Image src={deved} layout="fill" objectFit="cover"/> */}
         </div>
       </section>
 
